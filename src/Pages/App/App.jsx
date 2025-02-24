b0524d9d6f7668970e362500b8260399fe6b5cd6
Andrews <estudiapayaso@gmail.com>
2025-02-24 16:10:45 -0500

    Initial commit

---
+++ src/Pages/App/App.jsx
@@ -0,0 +1,40 @@
+//importaciones react
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import React from "react";
+
+//paginas
+import Home from "../Home/index";
+import MyAccount from "../MyAccount/index";
+import MyOrder from "../MyOrder/index";
+import MyOrders from "../MyOrders/index";
+import NotFound from "../NotFound/index";
+import SignIn from "../SignIn/index";
+import Navbar from "../../Components/Navbar";
+//importaciones css
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {path: "/", element: <Home />},
+    {path: "/my-account", element: <MyAccount />},
+    {path: "/my-order", element: <MyOrder />},
+    {path: "/my-orders", element: <MyOrders />},
+    {path: "/sign-in", element: <SignIn />},
+    {path: "/*", element: <NotFound />},
+
+
+  ]);
+  return routes;
+};
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <AppRoutes />
+        <Navbar />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
---
+++ src/Pages/MyOrders/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../../Components/Layout'
+
+function MyOrders
+() {
+  return (
+    <Layout>MyOrders</Layout>
+  )
+}
+
+export default MyOrders
