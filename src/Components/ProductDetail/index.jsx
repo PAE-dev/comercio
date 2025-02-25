4b6b49cd3d79d4ca9b7f2fe1edca58e8b57929ae
Andrews <estudiapayaso@gmail.com>
2025-02-25 10:01:26 -0500

    mostrando productos en Productdetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import { ShoppingCartContext } from "../../Context";
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 
 function ProductDetail() {
+
+  const context = useContext(ShoppingCartContext)
+
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
+    <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden' } product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
         <div className='flex justify-between items-center p-6'>
             <h2 className='font-medium text-xl'>Detail</h2>
             <div>
