f5812bd238849cd6ad6b6e605101bdbe6c53b70a
Andrews <estudiapayaso@gmail.com>
2025-02-24 20:15:27 -0500

    contador de productos en el carrito

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,5 +1,6 @@
 //importaciones react
 import { useRoutes, BrowserRouter } from "react-router-dom";
+import { ShoppingCartProvider } from "../../Context";
 import React from "react";
 
 //paginas
@@ -14,26 +15,24 @@ import Navbar from "../../Components/Navbar";
 
 const AppRoutes = () => {
   let routes = useRoutes([
-    {path: "/", element: <Home />},
-    {path: "/my-account", element: <MyAccount />},
-    {path: "/my-order", element: <MyOrder />},
-    {path: "/my-orders", element: <MyOrders />},
-    {path: "/sign-in", element: <SignIn />},
-    {path: "/*", element: <NotFound />},
-
-
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
   ]);
   return routes;
 };
 
 const App = () => {
   return (
-    <>
+    <ShoppingCartProvider>
       <BrowserRouter>
         <AppRoutes />
         <Navbar />
       </BrowserRouter>
-    </>
+    </ShoppingCartProvider>
   );
 };
 
