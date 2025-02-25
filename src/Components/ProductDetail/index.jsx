40ecad956fb099904cdb9c0aa093c6a1a0d93aa3
Andrews <estudiapayaso@gmail.com>
2025-02-25 09:37:12 -0500

    Maquetando el product detail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+
+import { XMarkIcon } from '@heroicons/react/24/solid'
 import './styles.css'
 
 function ProductDetail() {
   return (
     <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white'>
-        <div className='flex justify-between items-center'>
+        <div className='flex justify-between items-center p-6'>
             <h2 className='font-medium text-xl'>Detail</h2>
-            <div>X</div>
+            <div>
+                <XMarkIcon className="size-6 text-black"></XMarkIcon>
+            </div>
         </div>
     </aside>
   )
